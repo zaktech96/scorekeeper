11cbaed433aee7a331e48b6283705e106f993bba
zaktech96 <96632945+zaktech96@users.noreply.github.com>
2022-05-22 01:33:16 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,57 @@
+const p1 = {
+    score: 0,
+    button: document.querySelector('#p1Button'),
+    display: document.querySelector('#p1Display'),
+}
+
+const p2 = {
+    score: 0,
+    button: document.querySelector('#p2Button'),
+    display: document.querySelector('#p2Display'),
+}
+
+const resetButton = document.querySelector('#reset');
+const winningScoreSelect = document.querySelector('#playto');
+let winningScore = 3;
+let isGameover = false;
+
+function updateScores(player, opponent) {
+    if (!isGameover) true;
+    player.score += 1;
+    if (player.score === winningScore) {
+        isGameover = true;
+        player.display.classList.add('has-text-success');
+        opponent.display.classList.add('has-text-danger');
+        player.button.disabled = true;
+        opponent.button.disabled = true;
+    }
+    player.display.textContent = player.score;
+}
+
+p1.button.addEventListener('click', function () {
+    updateScores(p1, p2)
+})
+
+p2.button.addEventListener('click', function () {
+    updateScores(p2, p1)
+})
+
+winningScoreSelect.addEventListener('change', function () {
+    winningScore = parseInt(this.value);
+    reset();
+})
+
+
+
+resetButton.addEventListener('click', reset)
+
+function reset() {
+    isGameover = false;
+    for (let p of [p1, p2]) {
+        p.score = 0;
+        p.display.textContent = 0;
+        p.display.classList.remove('has-text-success', 'has-text-danger');
+        p.button.disabled = false;
+    }
+}
+
